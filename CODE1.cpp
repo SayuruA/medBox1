f91762d6c85ea22bcfbb34cee330fda959254215
SayuruA <sayurushenal141@gmail.com>
2024-03-10 02:19:55 +0530

    time keeping using milli func complete

--- CODE1.cpp
+++ CODE1.cpp
@@ -15,30 +15,81 @@
 
 Adafruit_SSD1306 display(SCRN_WIDTH, SCRN_HEIGHT, &Wire, OLED_RST);
 
+// time varibles
+int days;
+int hours;
+int minutes;
+int seconds; 
+unsigned long  timeNow;
+unsigned long  timeLast;
 
+
+
+
+void printOLED(String text, bool clear = true, int x=0, int y=0, int textSize = 1){
+    // clear the oled
+    if( clear ){ display.clearDisplay(); }
+
+    //diplay a message
+    display.setTextSize(textSize);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(x,y);
+    display.println(text);
+    display.display();
+}
+
+void printTime(){
+    printOLED(String(days));
+    printOLED(":",false,20,0);
+    printOLED(String(hours),false,30,0);
+    printOLED(":",false,50,0);
+    printOLED(String(minutes),false,60,0);
+    printOLED(":",false,80,0);
+    printOLED(String(seconds),false,90,0);
+}
+
+void update_time(){
+    timeNow = millis()/1000;
+    seconds = timeNow-timeLast;
+
+    if (seconds >= 60){
+        minutes += 1;
+        timeLast+=60;
+    }
+    if (minutes == 60){
+        hours+=1;
+        minutes=0;
+    }
+    if (hours == 24){
+        days += 1;
+        hours = 0;
+    }
+}
 
 void setup() {
-  
-  Serial.begin(115200);
-  if (!display.begin( SSD1306_SWITCHCAPVCC, SCRN_ADDR ) ){
-    Serial.println( F( "SSD1306 allocation failed" ) );
-    for(;;);
-  }
-  
+    
+    //initialize serial monitor and the oled
+    Serial.begin(115200);
+    if (!display.begin( SSD1306_SWITCHCAPVCC, SCRN_ADDR ) ){
+        Serial.println( F( "SSD1306 allocation failed" ) );
+        for(;;);
+    }
+    // turn on the oled
+    display.display();
+    delay(2000);
+    //
 
-  // turn on the oled
-  display.display();
-  delay(2000);
+
+
+    printOLED("Welcome to the MediBox");
+
 
   // clear the oled
   display.clearDisplay();
+    
+  // clear the oled
+  display.clearDisplay();
 
-  //diplay a message
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0,0);
-  display.println( F( "Welcome to Medibox!" ) );
-  display.display();
   
 }
 
@@ -46,3 +97,4 @@ void loop() {
  
   delay(10); // this speeds up the simulation
 }
+
