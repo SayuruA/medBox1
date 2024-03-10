38634fafbf61f7025b2d7cc151fad4271b02f2d7
Sayuru Amugoda <129970426+SayuruA@users.noreply.github.com>
2024-03-10 20:12:52 +0530

    Completed the Menu and setting time /alarm functionalities.
    Added more comment. More readability

--- CODE1.cpp
+++ CODE1.cpp
@@ -1,6 +1,12 @@
-///Libraries and Definitions
+// author: sayuruA/ 210041M
+// project: Smart MediBox/ EN2853
 
-//for I2C comm
+
+
+/// Libraries and Definitions *************************************************************************************************************
+
+
+// for I2C comm
 #include <Wire.h> 
 // libraries required for the oled programming
 #include <Adafruit_GFX.h>
@@ -18,12 +24,21 @@
 #define BUZZER 5
 #define LED_1 15
 #define PB_CANCEL 34 //indicator stop button
-///
+
+// navigate buttons
+#define PB_OK 32
+#define PB_UP 33
+#define PB_DOWN 35
+
+///**************************************************************************************************************************************///
+
+/// Gllobal Varibles ***********************************************************************************************************************
+
 
 //oled display object
 Adafruit_SSD1306 display(SCRN_WIDTH, SCRN_HEIGHT, &Wire, OLED_RST);
 
-// time varibles
+// time keeping varibles
 int days;
 int hours;
 int minutes;
@@ -32,13 +47,13 @@ unsigned long  timeNow;
 unsigned long  timeLast;
 
 // alarm variables
-bool alarm_enabled = true;
+bool alarm_enabled = true; // alarms enbled at the default times(below) in the begining
 int n_alarms = 3;
-int alarm_hours[] = {0, 1,2};
-int alarm_minutes[] = {1, 10,20};
+int alarm_hours[] = {0, 7, 19};//specify default alarms
+int alarm_minutes[] = {1, 0, 0};
 bool alarm_triggered[] = {false, false, false};
 
-// buzzer tones
+// buzzer variables/ tones 
 int n_notes = 8;
 int C = 262;
 int D = 294;
@@ -50,6 +65,14 @@ int B = 494;
 int C_H = 523;
 int notes[] = {C, D, E, F, G, A, B, C_H};
 
+// mode selection varibles
+int current_mode = 0;
+int max_modes = 6;
+String modes[] = {"1 - Set Time", "2 - Set Alarm 1", "3 - Set Alarm 2", "4 - Set Alarm 3", "5 - Disable Alarms", "6 - Set Time Zone"};
+
+///**************************************************************************************************************************************///
+
+/// Modules/ Functions ********************************************************************************************************************
 
 
 void printOLED(String text, bool clear = true, int x=0, int y=0, int textSize = 1){
@@ -131,26 +154,227 @@ void ring_alarm(){
     display.clearDisplay();
 }
 
+void go_to_menu(){
+    while (digitalRead(PB_CANCEL) == HIGH){
+        printOLED(modes[current_mode]);
+        int pressed = wait_for_button_press();
+        
+        if (pressed == PB_UP){
+            delay(200);
+            current_mode++;
+            current_mode = current_mode % max_modes;
+        }
+        else if (pressed == PB_DOWN){
+            delay(200);
+            current_mode--;
+            current_mode = current_mode % max_modes;
+            if (current_mode < 0){
+                current_mode = max_modes - 1;
+            }
+        }
+        else if (pressed == PB_OK){
+            delay(200);
+            run_mode(current_mode);
+            // Serial.println(current_mode); //Note that the
+        }
+        else if (pressed == PB_CANCEL){
+            delay(200);
+            break;
+        }
+    }
+}
+
+void run_mode(int mode){
+    if (mode == 0){
+        set_time();
+    }
+    else if (mode == 1 || mode == 2 || mode ==3){
+        set_alarm(mode - 1); 
+    }
+    else if (mode == 4){
+        alarm_enabled = false;
+        printOLED("Alarms disabled!");
+        delay(1000);
+    }
+    // else if (mode  == 5){
+    //     set_time_zone();
+    // }
+}
+
+int wait_for_button_press(){
+    while (true){
+        if (digitalRead(PB_UP) == LOW){
+            delay(200);
+            return PB_UP;
+        }
+        else if (digitalRead(PB_DOWN) == LOW){
+            delay(200);
+            return PB_DOWN;
+        }
+        else if (digitalRead(PB_OK) == LOW){
+            delay(200);
+            return PB_OK;
+        }
+        else if (digitalRead(PB_CANCEL) == LOW){
+            delay(200);
+            return PB_CANCEL;
+        }
+        update_time();
+    }
+}
+void set_time(){
+    int temp_hour = hours;
+    while (true){
+        
+        printOLED("Enter hour: " + String(temp_hour));
+        int pressed = wait_for_button_press();
+        if (pressed == PB_UP){
+            delay(200);
+            temp_hour++;
+            temp_hour = temp_hour % 24;
+        }
+        else if (pressed == PB_DOWN){
+            delay(200);
+            temp_hour--;
+            temp_hour = temp_hour % 24;
+            if (temp_hour < 0){
+                temp_hour = 23;
+            }
+        }
+        else if (pressed == PB_OK){
+            delay(200);
+            hours = temp_hour;
+            break;
+        }
+        else if (pressed == PB_CANCEL){
+            delay(200);
+            break;
+        }
+    }
+    
+    int temp_minute = minutes;
+    while (true){
+        
+        printOLED("Enter minute: " + String(temp_minute));
+        int pressed = wait_for_button_press();
+        if (pressed == PB_UP){
+            delay(200);
+            temp_minute++;
+            temp_minute = temp_minute % 60;
+        }
+        else if (pressed == PB_DOWN){
+            delay(200);
+            temp_minute--;
+            temp_minute = temp_minute % 60;
+            if (temp_minute < 0){
+                temp_minute = 59;
+            }
+        }
+        else if (pressed == PB_OK){
+            delay(200);
+            minutes = temp_minute;
+            break;
+        }
+        else if (pressed == PB_CANCEL){
+            delay(200);
+            break;
+        }
+    }
+   
+    printOLED("Time is set");
+    delay(1000);
+}
+
+void set_alarm(int alarm){
+    int temp_hour = alarm_hours[alarm];
+    while (true){
+        
+        printOLED("Enter hour: " + String(temp_hour));
+        int pressed = wait_for_button_press();
+        if (pressed == PB_UP){
+            delay(200);
+            temp_hour++;
+            temp_hour = temp_hour % 24;
+        }
+        else if (pressed == PB_DOWN){
+            delay(200);
+            temp_hour--;
+            temp_hour = temp_hour % 24;
+            if (temp_hour < 0){
+                temp_hour = 23;
+            }
+        }
+        else if (pressed == PB_OK){
+            delay(200);
+            alarm_hours[alarm] = temp_hour;
+            break;
+        }
+        else if (pressed == PB_CANCEL){
+            delay(200);
+            break;
+        }
+    }
+    
+    int temp_minute = alarm_minutes[alarm];
+    while (true){  
+        printOLED("Enter minute: " + String(temp_minute));
+        int pressed = wait_for_button_press();
+        if (pressed == PB_UP){
+            delay(200);
+            temp_minute++;
+            temp_minute = temp_minute % 60;
+        }
+        else if (pressed == PB_DOWN){
+            delay(200);
+            temp_minute--;
+            temp_minute = temp_minute % 60;
+            if (temp_minute < 0){
+                temp_minute = 59;
+            }
+        }
+        else if (pressed == PB_OK){
+            delay(200);
+            alarm_minutes[alarm] = temp_minute;
+            break;
+        }
+        else if (pressed == PB_CANCEL){
+            delay(200);
+            break;
+        }
+    }
+    printOLED("Alarm " + String(alarm));
+    delay(1000);
+}
+
+//////////////////////////***************************************************************************************************************///
 
 void setup() {
     
+
+    
+    //setup indicator pins
+    pinMode(BUZZER, OUTPUT);
+    pinMode(LED_1, OUTPUT);
+
+    // cancel /alarm stop button
+    pinMode(PB_CANCEL, INPUT);
+    // navigation buttons
+    pinMode(PB_DOWN, INPUT);
+    pinMode(PB_OK, INPUT);
+    pinMode(PB_UP, INPUT);
+
     //initialize serial monitor and the oled
     Serial.begin(115200);
     if (!display.begin( SSD1306_SWITCHCAPVCC, SCRN_ADDR ) ){
         Serial.println( F( "SSD1306 allocation failed" ) );
         for(;;);
     }
+
     // turn on the oled
     display.display();
     delay(2000);
-    
-    //setup indicator pins
-    pinMode(BUZZER, OUTPUT);
-    pinMode(LED_1, OUTPUT);
-    // cancel /alarm stop button
-    pinMode(PB_CANCEL, INPUT);
 
-    printOLED("Welcome to the MediBox");
+    printOLED("Welcome to MediBox!");
 
 
   
@@ -158,6 +382,12 @@ void setup() {
 
 void loop() {
 
+    
     update_time_with_check_alarm();
-
+    // check for MENU/ OK button
+    if (digitalRead(PB_OK) == LOW){
+        delay(200);
+        go_to_menu();
+    }
+    
 }
