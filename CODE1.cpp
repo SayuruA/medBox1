c7654251f25288e5364023262a3c48cf1d75d03b
Sayuru Amugoda <129970426+SayuruA@users.noreply.github.com>
2024-05-13 15:28:51 +0530

    added code parts for assignment 2

--- CODE1.cpp
+++ CODE1.cpp
@@ -14,6 +14,46 @@
 // for the Thermal/ Humidity sensor DHT22 
 #include <DHTesp.h>
 #include <WiFi.h>
+// for servo
+#include <ESP32Servo.h>
+// for MQTT
+#include <PubSubClient.h>
+// for wifi and Mqtt
+WiFiClient espClient;               // wifi client,  provides wifi signal (ininstance)
+PubSubClient mqttClient(espClient); // using the wifi instance,  creates an object named mqttClient of type PubSubClient, which is a library used for MQTT communication.
+
+// LDR
+#define LDR_1 36 // right 
+#define LDR_2 39 // left 
+
+// servo motor
+#define motorPin 17 // servo motor
+
+// for the motorangle calculations
+String msg;
+float motor_angle_offsets;
+float control_factor = 0.75; // controlling factor
+float min_angle = 30;        // minimum angle
+float motor_angle = 0;
+float D_servo;
+
+// needed to calculate light intensity
+float GAMMA = 0.75;
+const float RL10 = 50;
+float MIN_ANGLE = 30;
+float LUX1 = 0;
+float LUX2 = 0;
+
+String temp_value; // needed to save tempreature values
+
+// character arrays needed for publishing data to mqtt server
+char LDR_ar[6];
+char temp_ar[6];
+char high_ldr[38];
+
+// objects
+Servo servo; // setup servor motor
+
 
 // specify screen dimensions in pixel count
 #define SCRN_WIDTH 128
@@ -83,6 +123,17 @@ int current_mode = 0;
 int max_modes = 6;
 String modes[] = {"1 - Set Time", "2 - Set Alarm 1", "3 - Set Alarm 2", "4 - Set Alarm 3", "5 - Disable Alarms", "6 - Set Time Zone"};
 
+
+const char *MQTT_SERVER = "test.mosquitto.org";
+// Producer topics
+const char *LDR_TOPIC = "CURRENT_LDR_210035A";
+const char *SERVO_MOTOR_ANGLE_TOPIC = "SERVO_MOTOR_ANGLE";
+const char *TEMPERATURE_TOPIC = "CURRENT_TEMP_210035A";
+const char *MAX_LDR_TOPIC = "MAX_LDR_210035A";
+
+// Subscribe topics
+const char *SERVO_MIN_ANGLE_TOPIC = "MIN_ANGLE_210035A";
+const char *SERVO_CONTROL_FACTOR_TOPIC = "CONTROL_FACTOR_210035A";
 ///**************************************************************************************************************************************///
 
 
@@ -481,6 +532,162 @@ void check_temp(){
     }
 }
 
+// function for establishing mqtt connection
+void setupMqtt()
+{
+  mqttClient.setServer("test.mosquitto.org", 1883); // setup mqtt server, "test.mosquitto.org"
+  mqttClient.setCallback(receiveCallback);          // after subscribing, reciver call back recieves values
+}
+
+// function to attempt connection to MQTT broker and subscribes to topics
+void conect_to_broker()
+{
+
+  // check whether connected, and while it is not connected.....
+  while (!mqttClient.connected())
+  {
+    Serial.print("Attempting MQTT connection..."); //....print connecting
+    if (mqttClient.connect("ESP-8266-67895684567"))
+    {                            // ESP-8266-67895684567 is the id
+      Serial.print("connected"); // srver connection success
+
+      // subscribe to the below topics of Node red
+      mqttClient.subscribe(SERVO_CONTROL_FACTOR_TOPIC);
+      mqttClient.subscribe(SERVO_MIN_ANGLE_TOPIC); // best place to subscribe (incoming) is in connect to broker
+    }
+
+    //....if mqtt connection failed
+    else
+    {
+      Serial.print("failed; client state:");
+      Serial.println(mqttClient.state()); // mqtt client state gives what the problem in connection is
+      delay(3000);                        // wait 5s before attempting reconnection
+    }
+  }
+}
+
+// function for creating and selecting the maximum light inensity values
+void light_intensity(){
+  calculate_LDR_values(); // calculte LUX1 & LUX2
+  String str_temp;
+
+  // checking if intensity values are nan and if so, setting them to zero
+  // if nan, we can not compare
+  if (isnan(LUX1)){
+    LUX1 = 0;
+  }
+  if (isnan(LUX2)){
+    LUX2 = 0;
+  }
+
+  // comparing maximum light intensity values
+  // right ldr is higher
+  if (LUX1 > LUX2){
+    String(LUX1).toCharArray(LDR_ar, 6);
+    str_temp = "Left LDR < Right LDR";
+    str_temp.toCharArray(high_ldr, 38);
+  }
+  // left ldr is higher
+  else if (LUX2 > LUX1){
+    String(LUX2).toCharArray(LDR_ar, 6);
+    str_temp = "Left LDR > Right LDR";
+    str_temp.toCharArray(high_ldr, 38);
+  }
+  // both ldrs is have same value
+  else{
+    str_temp = "LDRs : Same Intensity";
+    str_temp.toCharArray(high_ldr, 38);
+  }
+}
+
+// funtion to calculate motor angle
+void sliding_window(){
+  calculate_LDR_values(); // calculte LUX1 & LUX2
+  if (isnan(LUX1)){
+    LUX1 = 0;
+  }
+  if (isnan(LUX2)){
+    LUX2 = 0;
+  }
+
+  if (LUX2 < LUX1){
+    D_servo = 1.5; // given in assignment
+  }
+  else if (LUX2 > LUX1){
+    D_servo = 0.5; // given in assignment
+  }
+
+  float max_intensity = max(LUX1, LUX2); // take the maximum of the 2 intensities as the maximum light intensity
+  if (min_angle != 0 && control_factor != 0){
+    motor_angle_offsets = min_angle;
+    motor_angle = min((motor_angle_offsets * D_servo) + (180 - motor_angle_offsets) * max_intensity * control_factor, (float)180); // calculate motor angel using the equation
+    Serial.print("motor_angle: ");
+    Serial.println(motor_angle);
+    servo.write(motor_angle); // set the servo motor's angel to the calculated value
+    delay(15);
+  }
+}
+
+// function to calculate maximum light intensities in 0-1 range
+void calculate_LDR_values(){
+
+  float volt_1 = analogRead(LDR_1) / 1024. * 5;
+  float R1 = 2000 * volt_1 / (1 - volt_1 / 5);
+  float max_LUX1 = pow(RL10 * 1e3 * pow(10, GAMMA) / 322.58, (1 / GAMMA)); // max intensity of light of right ldr
+  LUX1 = pow(RL10 * 1e3 * pow(10, GAMMA) / R1, (1 / GAMMA)) / max_LUX1;    // max light intensity (R) range 0-1
+
+  float volt_2 = analogRead(LDR_2) / 1024. * 5;
+  float R2 = 2000 * volt_2 / (1 - volt_2 / 5);
+  float max_LUX2 = pow(RL10 * 1e3 * pow(10, GAMMA) / 322.58, (1 / GAMMA)); // calculate max intensity of light of left ldr
+  LUX2 = pow(RL10 * 1e3 * pow(10, GAMMA) / R2, (1 / GAMMA)) / max_LUX2;    // max light intensity (L) range 0-1
+}
+
+// function to convert temreature values to character array
+void temp_change_to_char(void){
+  TempAndHumidity data = dhtSensor.getTempAndHumidity(); // get temp and humid values
+  delay(100);
+  temp_value = String(data.temperature, 2); // convert tempreature value of 2 decimal places to string
+  temp_value.toCharArray(temp_ar, 6);       // convert string into char array
+}
+
+// Receiving data from the server
+void receiveCallback(char *topic, byte *payload, unsigned int length){
+  Serial.print("message arrived [");
+  Serial.print(topic); // the topic of the rxed msg will display
+  Serial.print("] ");
+
+  char payloadCharAr[length]; // the payload that was recived, saved in payloadcharar, with the coming data size
+  for (int i = 0; i < length; i++){
+    Serial.print((char)payload[i]);
+    payloadCharAr[i] = (char)payload[i]; // convert each incoming data payload into character
+  }
+  Serial.println(); // print the recived data
+
+  String str_msg; // create a string variable
+
+  // if the recived message topic is SERVO_MIN_ANGLE_TOPIC
+  if (strcmp(topic, SERVO_MIN_ANGLE_TOPIC) == 0){
+    for (int i = 0; i < length; i++){
+      str_msg += (char)payload[i];   // create the string str_msg by concatinating each charcter of payload
+      min_angle = str_msg.toFloat(); // generate minimum angle floating point value, and set it to min_angle variable
+      delay(100);                    // Add delay for stabilization
+    }
+    Serial.print("Min angle: ");
+    Serial.println(min_angle);
+  }
+
+  // if incoming topic is SERVO_CONTROL_FACTOR_TOPIC
+  if (strcmp(topic, SERVO_CONTROL_FACTOR_TOPIC) == 0){
+    for (int i = 0; i < length; i++){
+      str_msg += (char)payload[i];        // create the string str_msg by concatinating each charcter of payload
+      control_factor = str_msg.toFloat(); // generate control factor floating point value, and set it to control_factor variable
+      delay(100);                         // Add delay for stabilization
+    }
+    Serial.print("Control factor: ");
+    Serial.println(control_factor);
+  }
+}
+
 
 //////////////////////////***************************************************************************************************************///
 
@@ -530,6 +737,9 @@ void setup() {
     printOLED("Welcome to MediBox!");
     delay(1000);
 
+    servo.attach(motorPin, 500, 2400);
+    setupMqtt();
+
 }
 
 
@@ -543,4 +753,21 @@ void loop() {
     }
     check_temp();
 
+    if (!mqttClient.connected()){
+        Serial.println("inside calling connect to broker");
+        conect_to_broker(); // if not connected, connect to mqtt using connect_to_broker() function
+    }
+
+    mqttClient.loop(); // ensure that the MQTT client continuously processes any incoming messages, publishes any outgoing messages, and maintains communication with the MQTT broker.
+
+    // publishes any available values while looping
+    mqttClient.publish(LDR_TOPIC, LDR_ar);
+    mqttClient.publish(MAX_LDR_TOPIC, high_ldr);
+    mqttClient.publish(TEMPERATURE_TOPIC, temp_ar);
+
+    delay(1000);
+
+    temp_change_to_char();
+    light_intensity();
+    sliding_window();
 }
